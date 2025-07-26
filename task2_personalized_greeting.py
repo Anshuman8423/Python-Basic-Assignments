b68fbceaad540b3f0e81399a6a96b42ec904065b
Anshuman Singh <anshuman945198@gmail.com>
2025-07-26 13:03:40 +0530

    Create task2_personalized_greeting.py

---
+++ task2_personalized_greeting.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+
+full_name = f"{first_name} {last_name}"
+
+print(f"\nHello {full_name}, welcome to Python programming!")
